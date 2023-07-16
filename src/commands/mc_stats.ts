67d867a21c89797644f59d3787f11c310869e267
KCGD <kcgdhosting@gmail.com>
2023-07-16 11:58:08 -0400

    added basic interaction w mc library for mc stats command

--- src/commands/mc_stats.ts
+++ src/commands/mc_stats.ts
@@ -1,11 +1,18 @@
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 import { Log } from "../lib/util/debug";
+import { GetEndpointStatus } from "../lib/util/minecraftServer";
+import { Endpoint } from "../main";
 
 export const MainCommand = {
     data: new SlashCommandBuilder()
-        .setName('mc-start')
-        .setDescription("Starts the minecraft server (WIP)"),
+        .setName('mc-status')
+        .setDescription("Gets the status of the minecraft server (WIP)"),
     execute: async (interaction:CommandInteraction) => {
-        await interaction.reply("Command not implemented");
+        if(Endpoint) {
+            await interaction.reply("Command in testing, see console");
+            GetEndpointStatus(Endpoint);
+        } else {
+            await interaction.reply("Endpoint variable is not defined. This is an issue with this bot's host. Please reach out to them.")
+        }
     }
 }
