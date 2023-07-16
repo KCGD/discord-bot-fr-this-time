d979a069839b61be2dcab425ddbe758b11250f87
KCGD <kcgdhosting@gmail.com>
2023-07-16 13:53:58 -0400

    added documentation and error codes to minecraftServer library

--- src/lib/util/minecraftServer.ts
+++ src/lib/util/minecraftServer.ts
@@ -1,6 +1,18 @@
+/* 
+    minecraftServer.ts
+
+    this library handles interactions with the minecraft server directly (via API over websocket),
+    and has two functions:
+        GetEndpointStatus - retrievs the current status of the minecraft server and returns either a StatusResponse object (given the operation succeeded) or an error code detailing what went wrong.
+        StartEndpointServer - sends a command to the minecraft server to start the actual minecraft server.   
+    This library only interacts with the minecraft servers' API and returns data elsewhere.
+    All discord interactions should be handled elsewhere.
+*/
+
 import { WebSocket } from "ws";
 import { Log } from "./debug";
 import { ProcessArgs } from "../../main";
+import { ErrorCode } from "./errors";
 
 
 //define types
@@ -16,9 +28,11 @@ type StatusResponse = {
     }
 }
 
+export interface StatusCallback {(error:ErrorCode|null, response:StatusResponse|null): any}
+
 
 //status command
-export function GetEndpointStatus(endpoint:string): any {
+export function GetEndpointStatus(endpoint:string, callback:StatusCallback): any {
     const socket:WebSocket = new WebSocket(endpoint);
     Log(`I`, ProcessArgs.debug, `Connecting to endpoint "${endpoint}" ...`);
 
@@ -34,7 +48,20 @@ export function GetEndpointStatus(endpoint:string): any {
     //socket recieve events - processing output
     socket.onmessage = function(message): void {
         //add try-parse, switch based on command responses
-        console.log(JSON.parse(message.data.toString()));
-        socket.close();
+        let response:StatusResponse|null = null;
+
+        //try parsing the server's response. If parse fails, return an error
+        try {
+            response = JSON.parse(message.data.toString()) as StatusResponse;
+            callback(null, response);
+        } catch (e) {
+            Log(`E`, false, `Failed to parse endpoint response data, ${e}`);
+            callback("PARSE_RESPONSE_FAILED", null);
+        }
+
+        //close socket after operations complete
+        if(socket.OPEN) {
+            socket.close();
+        }
     }
 }
