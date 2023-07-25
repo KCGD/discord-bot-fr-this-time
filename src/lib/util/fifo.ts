94305353bbef3c474eb74a0ffdf190614d170511
KCGD <kcgdhosting@gmail.com>
2023-07-25 19:31:24 -0400

    added fifo library

---
+++ src/lib/util/fifo.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+import { join, dirname } from 'path';
+import { ErrorCode } from './errors';
+import { execSync } from 'child_process';
+
+
+//function to create a fifo pipe
+export type createFifoOpts = {
+    overwrite?:boolean;
+    recursive?:boolean;
+}
+export function CreateFifo(path:string, options?:createFifoOpts): ErrorCode | void {
+    if(fs.existsSync(path)) {
+        if(options?.overwrite) {
+            fs.unlinkSync(path);
+        }
+
+        mkfifo(path);
+    } else {
+        if(!fs.existsSync(dirname(path))) {
+            fs.mkdirSync(dirname(path), {'recursive':options?.recursive});
+        }
+
+        mkfifo(path);
+    }
+}
+
+//helper to CreateFifo
+function mkfifo(path:string): void {
+    execSync(`mkfifo "${path}"`);
+}
