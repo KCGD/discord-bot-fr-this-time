89b3ff5c45a4d9bab56bd37abfdcd583f502c7a0
KCGD <kcgdhosting@gmail.com>
2023-07-25 16:53:28 -0400

    added testing mode warning

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,16 +1,21 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { Log } from './util/debug';
+import { bgBlue } from 'cli-color';
+
 
 //var for client status
 let clientReady:boolean = false;
 
+
 //create client object
 export const client:Client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates], });
+
 
 //export getter for client status
 export function ClientIsReady(): boolean {
     return clientReady;
 }
+
 
 //client login function
 export function Login(token:string): boolean {
@@ -18,7 +23,16 @@ export function Login(token:string): boolean {
     return true;
 }
 
+
 //client login event listener
 client.once(Events.ClientReady, c => {
     Log(`I`, false, `Logged in as ${c.user.tag}`);
+
+    //log testing warning message
+    if(c.user.tag.includes("TESTING")) {
+        Log(`W`, false, bgBlue(`
+        
+          WARNING: BOT IN TESTING MODE  
+        `));
+    }
 });
